b754940fcd27ebcdd375893fe3f1fe93b2776c89
José C.S. Curet <jcscuret@gmail.com>
2021-04-28 16:16:04 -0400

    reverseDigit() implemented
    given a number display its digit in reverse

--- Savitch - Chapter 14/reverseDigits.cpp
+++ Savitch - Chapter 14/reverseDigits.cpp
@@ -2,4 +2,22 @@
 
 // Write a recursive void function that has one parameter, which is a positive
 // integer. When called, the function writes its argument to the screen backward.
-// That is, if the argument is 1234, it outputs the following to the screen:
+// That is, if the argument is 1234, it outputs the following to the screen: 4321
+
+#include <iostream>
+using namespace std;
+
+void reverseDigits(int n){
+    
+    if(n < 10) cout << n << endl;
+    else{
+        cout << n % 10;
+        reverseDigits(n / 10);
+    }
+}
+
+int main(){
+
+    reverseDigits(1234);
+    return 0;
+}
